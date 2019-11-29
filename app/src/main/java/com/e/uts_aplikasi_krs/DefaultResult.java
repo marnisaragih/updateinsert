242f4a89953815d461a755f2205f5ff7d11dca2e
Marni <Marnisaragih06@gmail.com>
2019-11-29 19:27:11 +0700

    baru

---
+++ app/src/main/java/com/e/uts_aplikasi_krs/DefaultResult.java
@@ -0,0 +1,12 @@
+package com.e.uts_aplikasi_krs;
+
+class DefaultResult {
+    private String status;
+
+    public  String getStatus(){
+        return status;
+    }
+    public  void setStatus(String status){
+        this.status = status;
+    }
+}
--- app/src/main/java/com/e/uts_aplikasi_krs/MainActivity.java
+++ app/src/main/java/com/e/uts_aplikasi_krs/MainActivity.java
@@ -2,35 +2,45 @@ package com.e.uts_aplikasi_krs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.widget.Toast;
 
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
 
-    DataDosenService dataDosenService;
+    DataDosenService service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dataDosenService = RetrofitClient.getRetrofitInstance()
+        service = RetrofitClient.getRetrofitInstance()
                 .create(DataDosenService.class);
-        getAllDataDosen();
+        getAllDosen();
+        insertDosen();
+
     }
 
-    private void getAllDataDosen() {
-        Call<List<DataDosen>> call = dataDosenService.getDosenAll( "1");
+
+    private void getAllDosen() {
+        Call<List<DataDosen>> call = service.getDosenAll("1");
         call.enqueue(new Callback<List<DataDosen>>() {
             @Override
             public void onResponse(Call<List<DataDosen>> call, Response<List<DataDosen>> response) {
-                for(DataDosen dataDosen:response.body())
-                {
-                    System.out.println("Nama : "+dataDosen.getNama());
-                    System.out.println("Nidn : "+dataDosen.getNidn());
-    }
-}
+                for (DataDosen dataDosen : response.body()) {
+                    // System.out.println("Nama : "+dataDosen.getNama());
+                    // System.out.println("Nidn : "+dataDosen.getNidn());
+
+                }
+            }
+
             @Override
             public void onFailure(Call<List<DataDosen>> call, Throwable t) {
                 Toast.makeText(MainActivity.this, "Something wrong.....",
@@ -38,6 +48,43 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println(t.getMessage());
             }
         });
+    }
 
+    private void insertDosen() {
+        Call<DefaultResult> call = service.insertDosen("dendy", "001",
+                "jogja", "dendy@gmail.com", "skomeng", "dendy.jpg",
+                "1");
+        call.enqueue(new Callback<DefaultResult>() {
+            @Override
+            public void onResponse(Call<DefaultResult> call, Response<DefaultResult> response) {
+                System.out.println(response.body().getStatus());
+            }
+
+            @Override
+            public void onFailure(Call<DefaultResult> call, Throwable t) {
+                System.out.println("message:" + t.getMessage());
+                Toast.makeText(MainActivity.this, "Something went wrong... please try later",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            private void updateDosen() {
+                Call<DefaultResult> call = service.updateDosen("777222", "dendy",
+                        "001", "jogja", "dendy@gmail.com",
+                        "dendy.jpg", "001");
+                call.enqueue(new Callback<DefaultResult>() {
+                    @Override
+                    public void onResponse(Call<DefaultResult> call, Response<DefaultResult> response) {
+                        System.out.println(response.body().getStatus());
+                    }
+
+                    @Override
+                    public void onFailure(Call<DefaultResult> call, Throwable t) {
+                        System.out.println("message:" + t.getMessage());
+                        Toast.makeText(MainActivity.this, "sucsessful update",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
